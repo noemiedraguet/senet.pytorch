d8c1c3737f803f70c510b65ddc9ab8a49d7abb0b
noemiedraguet <80346540+noemiedraguet@users.noreply.github.com>
2025-04-04 13:43:27 +0200

    Update se_module.py threshold = 0.5

--- senet/se_module.py
+++ senet/se_module.py
@@ -2,7 +2,7 @@ from torch import nn
 
 
 class SELayer(nn.Module):
-    def __init__(self, channel, reduction=16, threshold = threshold):
+    def __init__(self, channel, reduction=16, threshold = 0.5):
         super(SELayer, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.fc = nn.Sequential(
