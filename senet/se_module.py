47e4a4f6f0c97fb89aba26bb32cb106f3452f98e
noemiedraguet <80346540+noemiedraguet@users.noreply.github.com>
2025-06-05 15:42:50 +0200

    Update se_module.py comments

--- senet/se_module.py
+++ senet/se_module.py
@@ -5,6 +5,7 @@ class SELayer(nn.Module):
     def __init__(self, channel, reduction=16, threshold = 0.5):
         super(SELayer, self).__init__()
         self.threshold = threshold
+        #The history attribute of each layer contains the average number of channels switched off in this layer for each image of the dataset
         self.history = []
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.fc = nn.Sequential(
@@ -14,20 +15,26 @@ class SELayer(nn.Module):
             nn.Sigmoid(),
             nn.Threshold(threshold, 0)
         )
+        #Initialisation of the mask that allows skipping residuals for channels put to 0
         self.mask = None
 
     def forward(self, x):
         b, c, _, _ = x.size()
         y = self.avg_pool(x).view(b, c)
         y = self.fc(y).view(b, c, 1, 1)
+        #The mask takes a value of 0 where the channel has a descriptor value of 0 and 1 otherwise.
         self.mask = (y != 0).float()
+        #y_simple contains a list where each element is relative to one image of the batch. Each element is a list of descriptors for each channel of the SELayer.
         y_simple = (y.view(b, c))
+        #y_bool replaces the values of y_simple with boolean values (true if the descriptor is 0, false otherwise).
         y_bool = y_simple == 0
         y_bool = y_bool.tolist()
         coverage_list = []
+        #For each element of y_bool (relative to one image), the coverage is the percentage of channels put to 0 for this image in this layer.
         for image_channels in y_bool:
             coverage = sum(image_channels)/len(image_channels)
             coverage_list.append(coverage)
+        #The percentage of switched off channels for each image is added to the history of the layer, to be able to get an average across the whole dataset later.
         for elem in coverage_list:
             self.history.append(elem)
         return x * y.expand_as(x)
