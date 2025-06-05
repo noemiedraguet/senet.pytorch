fcc326b4e8784cb148f88213a7ee7ff2086a21dd
noemiedraguet <80346540+noemiedraguet@users.noreply.github.com>
2025-06-05 15:50:12 +0200

    Update se_resnet.py comments

--- senet/se_resnet.py
+++ senet/se_resnet.py
@@ -147,11 +147,14 @@ def se_resnet152(num_classes=1_000):
 class CifarSEBasicBlock(nn.Module):
     def __init__(self, inplanes, planes, stride=1, reduction=16, threshold = 0.5):
         super(CifarSEBasicBlock, self).__init__()
+        #First convolutional layer
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
+        #Second convolutional layer
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = nn.BatchNorm2d(planes)
+        #SE Layer
         self.se = SELayer(planes, reduction, threshold)
         if inplanes != planes:
             self.downsample = nn.Sequential(nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False),
@@ -183,17 +186,23 @@ class CifarSEResNet(nn.Module):
     def __init__(self, block, n_size, num_classes=10, reduction=16, threshold = 0.5):
         super(CifarSEResNet, self).__init__()
         self.inplane = 16
+        #First convolutional layer
         self.conv1 = nn.Conv2d(
             3, self.inplane, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(self.inplane)
         self.relu = nn.ReLU(inplace=True)
+        #First stage
         self.layer1 = self._make_layer(
             block, 16, blocks=n_size, stride=1, reduction=reduction, threshold = threshold)
+        #Second stage
         self.layer2 = self._make_layer(
             block, 32, blocks=n_size, stride=2, reduction=reduction, threshold = threshold)
+        #Third stage
         self.layer3 = self._make_layer(
             block, 64, blocks=n_size, stride=2, reduction=reduction, threshold = threshold)
+        #Pooling
         self.avgpool = nn.AdaptiveAvgPool2d(1)
+        #Fully connected layer
         self.fc = nn.Linear(64, num_classes)
         self.initialize()
 
@@ -219,6 +228,7 @@ class CifarSEResNet(nn.Module):
         x = self.bn1(x)
         x = self.relu(x)
 
+        #3 stages
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
@@ -250,7 +260,7 @@ class CifarSEPreActResNet(CifarSEResNet):
         x = x.view(x.size(0), -1)
         x = self.fc(x)
 
-
+#Used model
 def se_resnet20(**kwargs):
     """Constructs a ResNet-18 model.
 
